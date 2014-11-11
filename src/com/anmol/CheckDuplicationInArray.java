c1667ee8be76f4f6cfa23051b0462c9fa20d15d1
Anmol More <anmolmore@gmail.com>
2014-11-11 15:38:50 +0530

    Three ways to check duplicates in array

--- src/com/anmol/CheckDuplicationInArray.java
+++ src/com/anmol/CheckDuplicationInArray.java
@@ -1,5 +1,44 @@
 package com.anmol;
 
-public class CheckDuplicationInArray {
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+/***
+ * source - http://javarevisited.blogspot.co.uk/2012/02/how-to-check-or-detect-duplicate.html
+ * @author Anmol More
+ *
+ */
+public class CheckDuplicationInArray {
+	
+	public static void main(String[] args) {
+		String[] withDuplicates = new String[] {"one","two","three","one"};
+        String[] withoutDuplicates = new String[] {"one","two","three"};
+
+        //bad way
+        for(int i=0;i<withDuplicates.length;i++) {
+        	for(int j=0;j<withDuplicates.length;j++) {
+        		if(withDuplicates[i].equals(withDuplicates[j]) && i!=j) {
+        			System.out.println("Duplicate found : " + withDuplicates[i]);
+        		}
+        	}
+        }
+        
+        //check duplicates using set
+        List arrayList = Arrays.asList(withDuplicates);
+        Set set = new HashSet(arrayList);
+        if(arrayList.size() != set.size())
+        	System.out.println("Array has duplicates");
+        
+      //check duplicates using set
+        Set mySet = new HashSet();
+        for(String item : withDuplicates) {
+        	if(mySet.contains(item)) {
+        		System.out.println("Duplicate element :: " + item);
+        	}
+        	else
+        		System.out.println(mySet.add(item));  //if item is added, its true ..
+        }
+	}
 }
